d51c9a049a12e02ca7d05ca33a19ae6d55acff99
Sangyeob Lee <sy0414@gmail.com>
2016-06-19 17:46:44 +0900

    login finished

--- static/afc/afc.js
+++ static/afc/afc.js
@@ -1,11 +1,47 @@
 Views = {};
 
+(function ($) {
+    $.fn.shake = function (options) {
+        // defaults
+        var settings = {
+            'shakes': 2,
+            'distance': 10,
+            'duration': 200
+        };
+        // merge options
+        if (options) {
+            $.extend(settings, options);
+        }
+        // make it so
+        var pos;
+        return this.each(function () {
+            $this = $(this);
+            // position if necessary
+            pos = $this.css('position');
+            if (!pos || pos === 'static') {
+                $this.css('position', 'relative');
+            }
+            // shake it
+            for (var x = 1; x <= settings.shakes; x++) {
+                $this.animate({ left: settings.distance * -1 }, (settings.duration / settings.shakes) / 4)
+                    .animate({ left: settings.distance }, (settings.duration / settings.shakes) / 2)
+                    .animate({ left: 0 }, (settings.duration / settings.shakes) / 4);
+            }
+        });
+    };
+}(jQuery));
+
 function changeView(section_name, view_name) {
-	Views[window.active_view].hide();
-	Views[section_name + view_name].show();
-	if(Views[window.active_view].section != section_name) {
-		$('section#' + Views[window.active_view].section).addClass('dnone');
+	if(window.active_view == undefined) {
 		$('section#' + section_name).removeClass('dnone');
+		Views[section_name + view_name].show();
+	} else {
+		Views[window.active_view].hide();
+		Views[section_name + view_name].show();
+		if(Views[window.active_view].section != section_name) {
+			$('section#' + Views[window.active_view].section).addClass('dnone');
+			$('section#' + section_name).removeClass('dnone');
+		}
 	}
 	window.active_view = section_name + view_name;
 }
@@ -52,6 +88,15 @@ addView('login', 'pop_login', function() {
 		changeView('login', 'default');
 	});
 	$view.find('button.login').on('touchstart', function() {
+		if($view.find('input[type=text]').val().trim() == '' || $view.find('input[type=password]').val().trim() == '') {
+			if($view.find('input[type=text]').val().trim() == '') {
+				$view.find('input[type=text]').parent().shake();
+			}
+			if($view.find('input[type=password]').val().trim() == '') {
+				$view.find('input[type=password]').parent().shake();
+			}
+			return false;
+		}
 		changeView('main', 'main');
 	});
 	$view.find('button.signup').on('touchstart', function() {
@@ -60,7 +105,7 @@ addView('login', 'pop_login', function() {
 }, function() {
 	$view = $('section#' + this.section + ' article.' + this.article);
 	$view.addClass('dnone');
-	$view.find('button.x_btn').off('touchstart');
+	$view.find('div.x_btn').off('touchstart');
 	$view.find('button.login').off('touchstart');
 	$view.find('button.signup').off('touchstart');
 });
@@ -83,7 +128,7 @@ addView('login', 'signup', function() {
 }, function() {
 	$view = $('section#' + this.section + ' article.' + this.article);
 	$view.addClass('dnone');
-	$view.find('button.x_btn').off('touchstart');
+	$view.find('div.x_btn').off('touchstart');
 	$view.find('button.facebook, button.kakao').off('touchstart');
 	$view.find('button.phone').off('touchstart');
 	$view.find('button.login').off('touchstart');
@@ -92,9 +137,21 @@ addView('login', 'signup', function() {
 addView('login', 'signup_phone', function() {
 	$view = $('section#' + this.section + ' article.' + this.article);
 	$view.removeClass('dnone');
+	$view.find('div.x_btn').on('touchstart', function() {
+		changeView('login', 'signup');
+	});
+	$view.find('button.signup').on('touchstart', function() {
+		if($view.find('input[type=text]').val().trim() == '') {
+			$view.find('input[type=text]').parent().shake();
+			return false;
+		}
+		changeView('main', 'main');
+	});
 }, function() {
 	$view = $('section#' + this.section + ' article.' + this.article);
 	$view.addClass('dnone');
+	$view.find('div.x_btn').off('touchstart');
+	$view.find('button.signup').off('touchstart');
 });
 
 addView('main', 'main', function() {
@@ -106,6 +163,6 @@ addView('main', 'main', function() {
 });
 
 $(document).ready(function() {
-	Views['logindefault'].show();
-	window.active_view = 'logindefault';
+	window.active_view = undefined;
+	changeView('main', 'main');
 });
