d22fd8cc26db8e5753921aecd186493ca11a1aed
Sangyeob Lee <sy0414@gmail.com>
2016-06-23 17:36:42 +0900

    comment feature added

--- static/afc/afc.js
+++ static/afc/afc.js
@@ -1,5 +1,102 @@
 Views = {};
+function formatDate(date, format, utc) {
+    var MMMM = ["\x00", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var MMM = ["\x01", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var dddd = ["\x02", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var ddd = ["\x03", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+    function ii(i, len) {
+        var s = i + "";
+        len = len || 2;
+        while (s.length < len) s = "0" + s;
+        return s;
+    }
+
+    var y = utc ? date.getUTCFullYear() : date.getFullYear();
+    format = format.replace(/(^|[^\\])yyyy+/g, "$1" + y);
+    format = format.replace(/(^|[^\\])yy/g, "$1" + y.toString().substr(2, 2));
+    format = format.replace(/(^|[^\\])y/g, "$1" + y);
+
+    var M = (utc ? date.getUTCMonth() : date.getMonth()) + 1;
+    format = format.replace(/(^|[^\\])MMMM+/g, "$1" + MMMM[0]);
+    format = format.replace(/(^|[^\\])MMM/g, "$1" + MMM[0]);
+    format = format.replace(/(^|[^\\])MM/g, "$1" + ii(M));
+    format = format.replace(/(^|[^\\])M/g, "$1" + M);
+
+    var d = utc ? date.getUTCDate() : date.getDate();
+    format = format.replace(/(^|[^\\])dddd+/g, "$1" + dddd[0]);
+    format = format.replace(/(^|[^\\])ddd/g, "$1" + ddd[0]);
+    format = format.replace(/(^|[^\\])dd/g, "$1" + ii(d));
+    format = format.replace(/(^|[^\\])d/g, "$1" + d);
+
+    var H = utc ? date.getUTCHours() : date.getHours();
+    format = format.replace(/(^|[^\\])HH+/g, "$1" + ii(H));
+    format = format.replace(/(^|[^\\])H/g, "$1" + H);
+
+    var h = H > 12 ? H - 12 : H == 0 ? 12 : H;
+    format = format.replace(/(^|[^\\])hh+/g, "$1" + ii(h));
+    format = format.replace(/(^|[^\\])h/g, "$1" + h);
+
+    var m = utc ? date.getUTCMinutes() : date.getMinutes();
+    format = format.replace(/(^|[^\\])mm+/g, "$1" + ii(m));
+    format = format.replace(/(^|[^\\])m/g, "$1" + m);
+
+    var s = utc ? date.getUTCSeconds() : date.getSeconds();
+    format = format.replace(/(^|[^\\])ss+/g, "$1" + ii(s));
+    format = format.replace(/(^|[^\\])s/g, "$1" + s);
+
+    var f = utc ? date.getUTCMilliseconds() : date.getMilliseconds();
+    format = format.replace(/(^|[^\\])fff+/g, "$1" + ii(f, 3));
+    f = Math.round(f / 10);
+    format = format.replace(/(^|[^\\])ff/g, "$1" + ii(f));
+    f = Math.round(f / 10);
+    format = format.replace(/(^|[^\\])f/g, "$1" + f);
+
+    var T = H < 12 ? "AM" : "PM";
+    format = format.replace(/(^|[^\\])TT+/g, "$1" + T);
+    format = format.replace(/(^|[^\\])T/g, "$1" + T.charAt(0));
+
+    var t = T.toLowerCase();
+    format = format.replace(/(^|[^\\])tt+/g, "$1" + t);
+    format = format.replace(/(^|[^\\])t/g, "$1" + t.charAt(0));
+
+    var tz = -date.getTimezoneOffset();
+    var K = utc || !tz ? "Z" : tz > 0 ? "+" : "-";
+    if (!utc) {
+        tz = Math.abs(tz);
+        var tzHrs = Math.floor(tz / 60);
+        var tzMin = tz % 60;
+        K += ii(tzHrs) + ":" + ii(tzMin);
+    }
+    format = format.replace(/(^|[^\\])K/g, "$1" + K);
+
+    var day = (utc ? date.getUTCDay() : date.getDay()) + 1;
+    format = format.replace(new RegExp(dddd[0], "g"), dddd[day]);
+    format = format.replace(new RegExp(ddd[0], "g"), ddd[day]);
+
+    format = format.replace(new RegExp(MMMM[0], "g"), MMMM[M]);
+    format = format.replace(new RegExp(MMM[0], "g"), MMM[M]);
+
+    format = format.replace(/\\(.)/g, "$1");
+
+    return format;
+};
+function toHHMMSS(sec) {
+    var sec_num = sec;
+    var milisec = Math.floor(sec_num % 1000);
+    sec_num /= 1000;
+    sec_num = Math.floor(sec_num);
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+    if (milisec < 10) milisec = "00"+milisec;
+    else if (milisec < 100) milisec = "0" + milisec;
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds+'.'+ milisec;
+    return time;
+}
 (function ($) {
     $.fn.shake = function (options) {
         // defaults
@@ -275,13 +372,35 @@ $(document).ready(function() {
 				}
 			}
 			for(i = 1; i <= 11; i ++) {
-				$view.find('div.img' + i).css('background-image', 'url(/static/afc/product/product-' + s + '-' + i + '.jpg)');
+				$view.find('div.img' + i).css('background-image', 'url(/static/afc/product/product-' + s + '-' + i + '.jpg)').attr('data-product-id', 'product-' + s + '-' + i);
 			}
+			$view.find('section.scrollable').scrollTop(0);
+		});
+		$view.find('div.product').on('click', function() {
+			var product = product_info[$(this).attr('data-product-id')];
+			$('article.product_detail section.product_image').css('background-image', $(this).css('background-image'));
+			$('article.product_detail section.desc div.loc').text(product.location);
+			$('article.product_detail header div.text').text(product.name);
+			$('article.product_detail section.desc div.shopname').text(product.name);
+			$('article.product_detail section.desc div.time').text(product.price);
+			changeView('main', 'product_detail');
 		});
 	}, function() {
 		$view = this.view;
 		$view.find('header img.icon_arrow').off('touchstart');
 		$view.find('nav ul li').off('click');
+		$view.find('div.product').off('click');
+	});
+
+	addView('main', 'product_detail', function() {
+		$view = this.view;
+		$view.find('header img.icon_arrow').on('touchstart', function() {
+			changeView('main', 'catalog');
+			return false;
+		});
+	}, function() {
+		$view = this.view;
+		$view.find('header img.icon_arrow').off('touchstart');
 	});
 
 	addView('main', 'qrscan', function() {
@@ -301,12 +420,52 @@ $(document).ready(function() {
 			changeView('main', 'main');
 			return false;
 		});
+		$view.find('div.commentdiv button').on('touchstart', function() {
+			if($view.find('input.author').val().trim() == '' || $view.find('input.comment').val().trim() == '') {
+				if($view.find('input.author').val().trim() == '') $view.find('input.author').shake(); 
+				if($view.find('input.comment').val().trim() == '') $view.find('input.comment').shake(); 
+				return false;
+			}
+			$.ajax({
+				url: '/afc/leave_comment/',
+				contentType: 'application/json;chatset=UTF-8',
+				method: 'POST',
+				data: JSON.stringify({
+					author: $view.find('input.author').val(),
+					comment: $view.find('input.comment').val(),
+					time: formatDate(new Date(), "yyyy-MM-dd<br />HH:mm")
+				}),
+				success: function(data, status, jqxhr) {
+					data = JSON.parse(data);
+					console.log(data);
+					var i;
+					$ul = $view.find('ul.comments');
+					$ul.find('li').remove();
+					for(i in data) {
+						var c = data[i];
+						var $newli = $('<li class="comment"><div class="author"></div><div class="comment"></div><div class="timestamp"></div></li>');
+						$newli.find('.author').text(c.author);
+						$newli.find('.comment').text(c.article);
+						$newli.find('.timestamp').html(c.timestamp);
+						$newli.appendTo($ul);
+					}	
+				}
+			});
+			$view.find('input.author').val('');
+			$view.find('input.comment').val('');
+			return false;
+		});
 	}, function() {
 		$view = this.view;
 		$view.find('header img.icon_arrow').off('touchstart');
 	});
 
 	window.active_view = undefined;
-	changeView('main', 'catalog');
-	//changeView('login', 'default');
+	if($('body').hasClass('startswith')) {
+		changeView($('body').attr('data-startwth-section'), $('body').attr('data-startwth-article'));
+	}
+	else {
+		changeView('main', 'project');
+		//changeView('login', 'default');
+	}
 });
---
+++ static/afc/product_info.js
@@ -0,0 +1,133 @@
+product_info = {
+	'product-1-1' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-2' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-3' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-4' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-5' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-6' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-7' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-8' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-9' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-10' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-1-11' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-1' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-2' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-3' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-4' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-5' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-6' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-7' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-8' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-9' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-10' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-2-11' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-1' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-2' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-3' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-4' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-5' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-6' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-7' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-8' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-9' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-10' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-3-11' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-1' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-2' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-3' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-4' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-5' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-6' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-7' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-8' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-9' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-10' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-4-11' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-1' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-2' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-3' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-4' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-5' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-6' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-7' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-8' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-9' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-10' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-5-11' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-1' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-2' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-3' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-4' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-5' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-6' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-7' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-8' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-9' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-10' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+	'product-6-11' : { name: '거실티비올려놓는거', price: '₩ 100,000', location: 'A-B-WonPat' },
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
